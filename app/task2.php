7523237d5fa55f9a5fb9c3cbdddfcddc68eb44d6
Valeriy Kuznetsov <vkuz.perm@gmail.com>
2020-02-16 13:48:02 +0500

    task2

---
+++ app/task2.php
@@ -0,0 +1,18 @@
+<?php
+
+function search_with_prefix($prefix, $strings){
+    $result = array();
+    $prefix_lenght = strlen($prefix);
+    foreach($strings as $str){
+        $str_prefix = substr(0, $prefix_lenght);
+        if($str_prefix == $prefix){
+            array_push($result, $str);
+        }
+    }
+}
+
+$arr = array("gsfgsdfg","sfgsgsdg","gsfdgsdg","sfgsfg")
+
+print(search_with_prefix("gsf", $arr));
+
+?>
