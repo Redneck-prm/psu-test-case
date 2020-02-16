ed966c7271cd8d1f062aba574ab01bdd80e1f503
Valeriy Kuznetsov <vkuz.perm@gmail.com>
2020-02-16 13:55:36 +0500

    fix

--- app/task2.php
+++ app/task2.php
@@ -3,10 +3,10 @@
 function search_with_prefix($prefix, $strings){
     $result = array();
     $prefix_lenght = strlen($prefix);
-    print("prefix_lenght ".$prefix_lenght);
+    print("prefix_lenght ".$prefix_lenght."\n");
     foreach($strings as $str){
         $str_prefix = substr(0, $prefix_lenght);
-        print("str_prefix ".$str_prefix." prefix ".$prefix);
+        print("str_prefix ".$str_prefix." prefix ".$prefix."\n");
         if($str_prefix == $prefix){
             array_push($result, $str);
         }
